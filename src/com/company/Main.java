31569f18c4fb8cdc9d09cbb77469597e7b60bc7c
dimansen68 <dimansen15@inbox.ru>
2020-12-11 16:29:58 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double price = 54.79;
+        int volume = 70;
+        double cost = price * volume;
+        System.out.println("Стоимость бензина: " + cost);
+//int gasCount = Integer.valueOf(args[0]);
+        //добавить switch
+
+    }
+}
